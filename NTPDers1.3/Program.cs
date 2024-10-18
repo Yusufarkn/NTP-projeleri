97562e25a0670847afec6514d80ea99ca8bba61f
Yusufarkn <147336381+Yusufarkn@users.noreply.github.com>
2024-10-18 15:17:36 +0300

    NTP1.3

---
+++ NTPDers1.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace NTPDers1._3
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            int sonuc = 0;
+            Console.WriteLine("Bir sayı giriniz: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 2)
+            {
+                Console.WriteLine("Sonuç:" + sonuc);
+                Console.ReadLine();
+            }
+           
+            for (int i = 2; i <= n; i++)
+            {
+                if (AsalMi(i))
+                {
+                    sonuc += i;
+                }
+            }
+            Console.WriteLine("1 den girdiğiniz sayıya kadar asal olan sayıların toplamı:" + sonuc);
+            Console.ReadLine();
+        }
+        // Asal sayı kontrol fonksiyonu
+        static bool AsalMi(int sayi)
+        {
+
+            for (int i = 2; i*i <= sayi; i++)
+            {
+                if (sayi % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
