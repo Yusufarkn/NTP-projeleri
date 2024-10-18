2574d044d548a6cf88297de43448263cffc1bbd7
Yusufarkn <147336381+Yusufarkn@users.noreply.github.com>
2024-10-18 15:15:09 +0300

    Adam Asmaca

---
+++ AdamAsmaca/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System.Drawing;
+
+namespace AdamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(349, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ŞEHRİ BUL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 234);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(71, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Harf giriniz:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(71, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Kalan Hakkınız:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(73, 414);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kullanılan harfler:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(667, 391);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 63);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Yeniden Başlat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(856, 502);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(234, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(373, 53);
+            this.label5.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(468, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 501);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ AdamAsmaca/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace AdamAsmaca
+{
+    public partial class Form1 : Form
+    {   
+       private string[] Sehirler = {
+        "ADANA", "ADIYAMAN", "AFYONKARAHİSAR", "AĞRI", "AMASYA", "ANKARA", "ANTALYA",
+        "ARTVİN", "AYDIN", "BALIKESİR", "BİLECİK", "BİNGÖL", "BİTLİS", "BOLU", "BURDUR",
+        "BURSA", "ÇANAKKALE", "ÇANKIRI", "ÇORUM", "DENİZLİ", "DİYARBAKIR", "EDİRNE",
+        "ELAZIĞ", "ERZİNCAN", "ERZURUM", "ESKİŞEHİR", "GAZİANTEP", "GİRESUN", "GÜMÜŞHANE",
+        "HAKKARİ", "HATAY", "ISPARTA", "MERSİN", "İSTANBUL", "İZMİR", "KARS", "KASTAMONU",
+        "KAYSERİ", "KIRKLARELİ", "KIRŞEHİR", "KOCAELİ", "KONYA", "KÜTAHYA", "MALATYA",
+        "MANİSA", "KAHRAMANMARAŞ", "MARDİN", "MUĞLA", "MUŞ", "NEVŞEHİR", "NİĞDE",
+        "ORDU", "RİZE", "SAKARYA", "SAMSUN", "SİİRT", "SİNOP", "SİVAS", "TEKİRDAĞ",
+        "TOKAT", "TRABZON", "TUNCELİ", "ŞANLIURFA", "UŞAK", "VAN", "YOZGAT", "ZONGULDAK",
+        "AKSARAY", "BAYBURT", "KARAMAN", "KIRIKKALE", "BATMAN", "ŞIRNAK", "BARTIN",
+        "ARDAHAN", "IĞDIR", "YALOVA", "KARABÜK", "KİLİS", "OSMANİYE", "DÜZCE"
+        };
+    
+        private string secilenSehir; // Rastgele seçilen şehir
+        private string gizliSehir;   // Şehrin _ ile gizlenmiş hali
+        private char[] kapalıHali; // Alt tireler ve bulunan harfler için dizi
+        private int kalanHak = 5;    // Başlangıçta 5 hak veriyoruz
+        private string kullanilanHarfler = ""; // Kullanılan harfleri tutacağız
+
+
+        public Form1()
+        {
+            InitializeComponent();           
+            RastgeleSehirSec();
+            HarfTireEkle();
+            kalanHak = 5; 
+            kullanilanHarfler = ""; 
+            label4.Text = "Kullanılan Harfler: ";
+            KalanHakGoster(); 
+        }
+
+        // Rastgele bir şehir seçiyoruz
+        private void RastgeleSehirSec()
+        {
+                Random rnd = new Random();
+                int rastgeleIndex = rnd.Next(Sehirler.Length); // 0 ile 80 arasında bir değer oluşturma şehir dizisinden 
+                secilenSehir = Sehirler[rastgeleIndex];
+
+                // Seçilen şehrin uzunluğuna göre kapalıHali'i başlatıyoruz
+                kapalıHali = new string('_', secilenSehir.Length).ToCharArray(); 
+            }
+
+
+
+        // Seçilen şehrin harf sayısı kadar alt tire ekle
+        private void HarfTireEkle() 
+        {
+            // kapalıHali dizisini boşluklarla birleştirip label5'e yazdırıyoruz
+            gizliSehir = string.Join(" ", kapalıHali);
+            label5.Text = gizliSehir;
+            label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter; //Ortalama işlemi
+        }
+
+        // TextBoxa girilen harfi kontrol etmek
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string girilenHarf = textBox1.Text.ToUpper(); // girilen harfleri büyük alma
+            textBox1.Text = ""; // TextBoxı temizlemek
+
+            if (string.IsNullOrEmpty(girilenHarf)) return; // Boş giriş olursa işlem yapma
+
+            if (kullanilanHarfler.Contains(girilenHarf))
+            {
+                MessageBox.Show("Bu harfi zaten kullandınız!");
+                return;
+            }
+
+            kullanilanHarfler += girilenHarf + " "; // Kullanılan harfleri al ve göster
+            label4.Text = "Kullanılan Harfler: " + kullanilanHarfler;
+
+            bool dogruMu = false;
+
+            // Şehir için girilen harfi kontrol et
+            for (int i = 0; i < secilenSehir.Length; i++)
+            {
+                if (secilenSehir[i] == girilenHarf[0]) // Harf doğru mu kontrol
+                {
+                    kapalıHali[i] = secilenSehir[i]; // Doğru harfi alt tire yerine koyma kısmı
+                    dogruMu = true;
+                }
+            }
+
+            if (dogruMu)
+            {
+                HarfTireEkle(); // Doğru harfleri gösterme
+                if (!kapalıHali.Contains('_'))
+                {
+                    MessageBox.Show("Kazandınızzz");
+                }
+            }
+            else
+            {
+                kalanHak--; // Yanlış harf girildiğine deneme hakkını azaltır
+                KalanHakGoster(); // Kalan hakları güncelleme işlemi
+                if (kalanHak == 0)
+                {
+                    MessageBox.Show("Kaybettin! aranan şehir:  " + secilenSehir);
+                    RastgeleSehirSec(); // Yeni şehir seçme
+                    HarfTireEkle();
+                    kalanHak = 5; // Hakları yenileme
+                    kullanilanHarfler = ""; // Kullanılan harfleri sıfırlamak
+                    label4.Text = "Kullanılan Harfler: ";
+                    KalanHakGoster(); // Kalan hakları güncelleme
+                }
+            }
+        }
+
+
+        private void KalanHakGoster()
+        {
+            label3.Text = "Kalan Hakkınız: " + kalanHak;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Formu ekrana gösterme
+        }
+
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RastgeleSehirSec();   // yeni şehir
+            HarfTireEkle();       // Yeni şehrin alt tire kısmı
+
+            kalanHak = 5;         // Kalan hakları sıfırla (ilk hale çevirmek)
+            kullanilanHarfler = ""; // Kullanılan harfleri sıfırla
+            label4.Text = "Kullanılan Harfler: ";  // Kullanılan harfler label'ını sıfırla
+
+            KalanHakGoster();     // Kalan hakları ekranda güncelle
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
