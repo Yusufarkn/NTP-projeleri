f2346ab352141bd5cdd381c2ec75e514d130b213
Yusufarkn <147336381+Yusufarkn@users.noreply.github.com>
2024-10-30 00:08:36 +0300

    NTP2.0

---
+++ NTP2.0/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NTP2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Aranilacaklar = new List<int>();
+            int input;
+
+            // Kullanıcıdan "0" girene kadar değer alma
+            Console.WriteLine("Dizinin elemanlarını giriniz (0 girildiğinde dizi sonlanacaktır): ");
+            while (true)
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == 0)
+                    break;
+                Aranilacaklar.Add(input);
+            }
+
+            // Kullanıcıdan aranacak sayıyı alma
+            Console.WriteLine("Aramak istediğiniz sayıyı giriniz: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            // Listeyi diziye çevirip sıralama yapma
+            int[] AranilacakDizi = Aranilacaklar.ToArray();
+            Array.Sort(AranilacakDizi);
+
+            // Binary search için değişken tanımları
+            int low = 0, high = AranilacakDizi.Length - 1;
+            bool found = false;
+
+            // Binary search algoritması
+            while (low <= high)
+            {
+                int index = (low + high) / 2;
+
+                if (AranilacakDizi[index] == s)
+                {
+                    found = true;
+                    Console.WriteLine($"Aranan değer {s} bulundu: İndeks {index}");
+                    Console.ReadLine();
+                    break;
+                }
+                else if (AranilacakDizi[index] < s)
+                {
+                    low = index + 1;
+                }
+                else
+                {
+                    high = index - 1;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine($"Aranan değer {s} bulunamadı.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
