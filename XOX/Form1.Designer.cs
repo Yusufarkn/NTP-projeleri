c04251d47fb6ea7de9dab947e74daf428f363f68
Yusufarkn <147336381+Yusufarkn@users.noreply.github.com>
2024-11-06 00:40:39 +0300

    XOX

---
+++ XOX/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace XOX
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label_SıraKimde = new System.Windows.Forms.Label();
+            this.Button_YndnBşlt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Location = new System.Drawing.Point(178, 152);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 75);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn2.ForeColor = System.Drawing.Color.Black;
+            this.btn2.Location = new System.Drawing.Point(250, 152);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 75);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn3.ForeColor = System.Drawing.Color.Black;
+            this.btn3.Location = new System.Drawing.Point(322, 152);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 75);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn4.ForeColor = System.Drawing.Color.Black;
+            this.btn4.Location = new System.Drawing.Point(178, 224);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 75);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn5.ForeColor = System.Drawing.Color.Black;
+            this.btn5.Location = new System.Drawing.Point(250, 224);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 75);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn6.ForeColor = System.Drawing.Color.Black;
+            this.btn6.Location = new System.Drawing.Point(322, 224);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 75);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn7.ForeColor = System.Drawing.Color.Black;
+            this.btn7.Location = new System.Drawing.Point(178, 296);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 75);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn8.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn8.ForeColor = System.Drawing.Color.Black;
+            this.btn8.Location = new System.Drawing.Point(250, 296);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 75);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn9.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn9.ForeColor = System.Drawing.Color.Black;
+            this.btn9.Location = new System.Drawing.Point(322, 296);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 74);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label_SıraKimde
+            // 
+            this.label_SıraKimde.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label_SıraKimde.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_SıraKimde.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label_SıraKimde.Location = new System.Drawing.Point(236, 109);
+            this.label_SıraKimde.Name = "label_SıraKimde";
+            this.label_SıraKimde.Size = new System.Drawing.Size(100, 25);
+            this.label_SıraKimde.TabIndex = 9;
+            // 
+            // Button_YndnBşlt
+            // 
+            this.Button_YndnBşlt.BackColor = System.Drawing.SystemColors.ControlText;
+            this.Button_YndnBşlt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button_YndnBşlt.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Button_YndnBşlt.ForeColor = System.Drawing.Color.Yellow;
+            this.Button_YndnBşlt.Location = new System.Drawing.Point(250, 451);
+            this.Button_YndnBşlt.Name = "Button_YndnBşlt";
+            this.Button_YndnBşlt.Size = new System.Drawing.Size(93, 50);
+            this.Button_YndnBşlt.TabIndex = 10;
+            this.Button_YndnBşlt.Text = "Yeniden Başlat";
+            this.Button_YndnBşlt.UseVisualStyleBackColor = false;
+            this.Button_YndnBşlt.Click += new System.EventHandler(this.Button_YndnBşlt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.Location = new System.Drawing.Point(210, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 33);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "TİC-TAC-TOE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::XOX.Properties.Resources.XOX;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(588, 557);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button_YndnBşlt);
+            this.Controls.Add(this.label_SıraKimde);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "XOX";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label label_SıraKimde;
+        private System.Windows.Forms.Button Button_YndnBşlt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ XOX/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace XOX
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; // true = X, false = O
+        int turnCount = 0; // hamle sayısı
+
+        public Form1()
+        {
+            InitializeComponent();
+            label_SıraKimde.Text = "Sıra: X";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ButtonClick(btn9);
+        }
+
+        private void ButtonClick(Button button)
+        {
+            // X veya O yerleştirme
+            if (turn)
+                button.Text = "X";
+            else
+                button.Text = "O";
+
+            // Sıra değişimi ve hamle sayısını arttırma
+            turn = !turn;
+            button.Enabled = false;
+            turnCount++;
+
+            label_SıraKimde.Text = turn ? "Sıra: X" : "Sıra: O";
+
+            // Kazanan kontrolü
+            CheckForWinner();
+        }
+
+        private void CheckForWinner()
+        {
+            bool thereIsAWinner = false;
+            string currentPlayer = turn ? "O" : "X"; // Çünkü hamleden sonra sıradaki oyuncu değişir
+
+            // Kazanma kombinasyonları
+            if ((btn1.Text == currentPlayer && btn2.Text == currentPlayer && btn3.Text == currentPlayer) ||
+                (btn4.Text == currentPlayer && btn5.Text == currentPlayer && btn6.Text == currentPlayer) ||
+                (btn7.Text == currentPlayer && btn8.Text == currentPlayer && btn9.Text == currentPlayer) ||
+                (btn1.Text == currentPlayer && btn4.Text == currentPlayer && btn7.Text == currentPlayer) ||
+                (btn2.Text == currentPlayer && btn5.Text == currentPlayer && btn8.Text == currentPlayer) ||
+                (btn3.Text == currentPlayer && btn6.Text == currentPlayer && btn9.Text == currentPlayer) ||
+                (btn1.Text == currentPlayer && btn5.Text == currentPlayer && btn9.Text == currentPlayer) ||
+                (btn3.Text == currentPlayer && btn5.Text == currentPlayer && btn7.Text == currentPlayer))
+            {
+                thereIsAWinner = true;
+            }
+
+            if (thereIsAWinner)
+            {
+                DisableButtons();
+                MessageBox.Show(currentPlayer + " Kazandı!", "Oyun Bitti");
+            }
+            else if (turnCount == 9)
+            {
+                MessageBox.Show("Berabere!", "Oyun Bitti");
+            }
+        }
+
+        private void DisableButtons()
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+        }
+
+        private void ResetGame()
+        {
+            turn = true;
+            turnCount = 0;
+            label_SıraKimde.Text = "Sıra: X";
+
+            btn1.Enabled = true; btn1.Text = "";
+            btn2.Enabled = true; btn2.Text = "";
+            btn3.Enabled = true; btn3.Text = "";
+            btn4.Enabled = true; btn4.Text = "";
+            btn5.Enabled = true; btn5.Text = "";
+            btn6.Enabled = true; btn6.Text = "";
+            btn7.Enabled = true; btn7.Text = "";
+            btn8.Enabled = true; btn8.Text = "";
+            btn9.Enabled = true; btn9.Text = "";
+        }
+
+        private void Button_YndnBşlt_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+    }
+}
